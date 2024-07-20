71d797f21bdb0f33ea1ec321ec4d0ccbdd05d41b
Jessé <jessepescador22@gmail.com>
2024-07-20 12:53:27 -0300

    realizando desafio react

--- 1_intro/Hello-world/src/App.jsx
+++ 1_intro/Hello-world/src/App.jsx
@@ -1,16 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <h1>Hello world</h1>
-    </>
-  )
+    <div className="App">
+      <h1>Hello world React</h1>
+      <h2>Emmet funcionou</h2>
+      <p className="paragrafo">
+        Fazendo a Resolução do Primeiro desafio do curso, que são três coisa que é preciso
+        fazer, primeira tenho que adcionar um elemento(html) no meu app.js, segundo tenho 
+        que criar uma regra de estilo pra esse elemento e a alterar a cor dele e por ultimo 
+        tenho que no meu index.html alerar o título da meta tag; 
+      </p>
+      
+    </div>
+  );
 }
 
-export default App
+export default App;
+
